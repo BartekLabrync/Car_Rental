e97e26a039ec59cf0a4c809522e589f835804f13
ololip <oliwerlipinski96@gmail.com>
2023-07-25 18:40:23 +0200

    refactor

--- src/main/java/com/example/rentcarapp/services/DataInit.java
+++ src/main/java/com/example/rentcarapp/services/DataInit.java
@@ -1,27 +1,39 @@
 package com.example.rentcarapp.services;
 
+import com.example.rentcarapp.models.Branch;
+import com.example.rentcarapp.models.RentCar;
 import com.example.rentcarapp.models.Reservations;
+import com.example.rentcarapp.models.User;
+import com.example.rentcarapp.repositories.BranchRepository;
+import com.example.rentcarapp.repositories.RentCarRepository;
 import com.example.rentcarapp.repositories.ReservationsRepository;
+import com.example.rentcarapp.repositories.UserRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.io.Console;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Component
 @AllArgsConstructor
 public class DataInit {
 
     private final ReservationsRepository reservationsRepository;
+    private final UserRepository userRepository;
+    private final RentCarRepository rentCarRepository;
+    private final BranchRepository branchRepository;
 
     @PostConstruct
-
     public void init () {
         Reservations reservation = Reservations.builder()
-                .amount(BigDecimal.valueOf(123))
+                .amount(BigDecimal.valueOf(199))
+                .reservDate(LocalDate.now())
+                .dateFor(LocalDate.now())
+                .dateTo(LocalDate.now())
                 .build();
-
-        reservationsRepository.save(reservation);
+        //reservationsRepository.save(reservation);
     }
 
 
